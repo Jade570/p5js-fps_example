dfd85e8533f16f1cb212b7ba2d02853058a9f079
Jade570 <44695194+Jade570@users.noreply.github.com>
2020-05-25 21:14:43 +0900

    Update example files

--- fps/fps.js
+++ fps/fps.js
@@ -1,5 +1,19 @@
 //range of rad from json should be 0.15<n<1
-class target {
+
+//aim-point variables
+let aim_rad, aim_x, aim_y, aim_z, aim_tipangle;
+
+//camera variables
+let cam_x, cam_y, cam_z;
+let cam_dx, cam_dy, cam_dz;
+let cam_cx, cam_cy, cam_cz;
+let pan, tilt;
+
+//user variables
+let sensitivity;
+
+
+class Target {
   constructor(jsonobj) {
     this.appear_time = jsonobj.appear_time;
     this.rad = (-cam_z) * jsonobj.rad;
@@ -7,16 +21,15 @@ class target {
     this.pos = createVector(cam_x + (this.rad) * sin(radians(jsonobj.pan)), cam_y + (this.rad) * sin(radians(jsonobj.tilt)), cam_z + (this.rad) * cos(radians(jsonobj.pan)) * cos(radians(jsonobj.tilt)));
     this.width = jsonobj.width/2;
     this.diagonal = this.width * sqrt(2);
-    this.centeranglex = atan((this.origin.x - cam_x) / (this.origin.z - cam_z));
-    this.centerangley = atan((this.origin.y - cam_y) / (this.origin.z - cam_z));
-    this.distance = sqrt(sq(this.origin.x - cam_x) + sq(this.origin.z - cam_z));
+    this.centeranglex = atan((this.pos.x - cam_x) / (this.pos.z - cam_z));
+    this.centerangley = atan((this.pos.y - cam_y) / (this.pos.z - cam_z));
+    this.distance = sqrt(sq(this.pos.x - cam_x) + sq(this.pos.z - cam_z));
     this.tipangle = asin(this.diagonal / this.distance);
     this.leftangle = this.centeranglex - this.tipangle;
     this.rightangle = this.centeranglex + this.tipangle;
     this.upangle = this.centerangley + this.tipangle;
     this.downangle = this.centerangley - this.tipangle;
 
-    this.texture = jsonobj.texture;
     this.detect;
     this.isrendering;
   }
@@ -26,7 +39,6 @@ class target {
         this.isrendering = true;
         push();
         tint(0, 255, 0);
-        texture(peopletexture[this.texture]);
         translate(this.pos);
         box(50, 50, 50);
         pop();
@@ -44,4 +56,21 @@ class target {
     }
     return this.detect;
   }
+}
+
+function updateCamCenter() {
+  cam_dz = cos(pan) * cos(tilt);
+  cam_dx = sin(pan);
+  cam_dy = sin(tilt);
+
+  // compute scene center position
+  cam_cx = cam_x + cam_dx * (-cam_z);
+  cam_cy = cam_y + cam_dy * (-cam_z);
+  cam_cz = cam_z + (cam_dz) * (-cam_z);
+
+  //compute aiming point position
+  aim_x = cam_x + cam_dx * aim_rad;
+  aim_y = cam_y + cam_dy * aim_rad;
+  aim_z = cam_z + (cam_dz) * aim_rad;
+  aim_tipangle = asin(0.25 / sqrt(sq(aim_x - cam_x) + sq(aim_z - cam_z)));
 }
--- sketch.js
+++ sketch.js
@@ -1,26 +1,88 @@
-let aim_rad, aim_x, aim_y, aim_z, aim_tipangle;
-let target = [];
-let json;
+let target = []; //array of target objects
+let json; //json file
+let sensitivity_slider; //a visual graph of mouse sensitivity
 
-function preload(){
+function preload() {
   json = loadJSON("fps/targets.json");
 }
 
-function setup(){
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+
+  //camera set-up
+  cam_x = 0;
+  cam_y = 0;
+  cam_z = -(windowHeight / 2 / tan(PI * 30.0 / 180.0));
+  cam_dx = 0;
+  cam_dy = 0;
+  cam_dz = 0;
+  pan = 0;
+  tilt = 0;
+
+  //aim point set-up
+  aim_rad = (-cam_z) / 10;
+
+  //load JSON datas into target objects
   for (let i = 0; i < Object.keys(json).length; i++) {
-    target[i] = new People(object[i]);
+    target[i] = new Target(json[i]);
     target[i].detect = false;
   }
+
+  //default mouse sensitivity
+  sensitivity = 8;
+
+  //sensitivity slider (doesn't work in mouse, but shows current sensitivity.)
+  //you can control sensitivity by UP/DOWN arrow key.
+  sensitivity_slider = createSlider(1, 20);
+  sensitivity_slider.position(10, 10);
 }
 
-function draw(){
+function draw() {
+  background(0);
+  lights();
+
+  //render target objects
   for (let i = 0; i < Object.keys(json).length; i++) {
     target[i].render();
   }
+
+  //camera set-up
+  updateCamCenter();
+  camera(cam_x, cam_y, cam_z, cam_cx, cam_cy, cam_cz, 0, -1, 0);
+  pan += radians(movedX) / sensitivity;
+  tilt -= radians(movedY) / sensitivity;
+
+  //aimpoint set-up
+  push();
+  translate(aim_x, aim_y, aim_z);
+  fill(255, 255, 255);
+  noStroke();
+  sphere(0.5, 4, 4);
+  pop();
+
+
+  //change sensitivity (by UP/DOWN arrows)
+  if (keyIsPressed) {
+    if (keyCode == UP_ARROW) {
+      sensitivity -= 0.1;
+      if (sensitivity >= 20) {
+        sensitivity = 20;
+      }
+    } else if (keyCode == DOWN_ARROW) {
+      sensitivity += 0.1;
+      if (sensitivity <= 1) {
+        sensitivity = 1;
+      }
+    }
+  }
+  sensitivity_slider.value(16 - sensitivity);
 }
 
-function mouseClicked(){
+function mouseClicked() {
+  //check detection
   for (let i = 0; i < Object.keys(json).length; i++) {
     target[i].detected();
   }
+  //lock pointer
+  requestPointerLock();
 }
